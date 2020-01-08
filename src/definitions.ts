d8a8b2844eb81dbe7bf3b46bec70a8622493731d
郑欣 <zhengxin@zhengxindeiMac.local>
2020-01-08 20:30:07 +0800

    add original files.

---
+++ src/definitions.ts
@@ -0,0 +1,9 @@
+declare module "@capacitor/core" {
+  interface PluginRegistry {
+    ZxCapPlugin: ZxCapPluginPlugin;
+  }
+}
+
+export interface ZxCapPluginPlugin {
+  echo(options: { value: string }): Promise<{value: string}>;
+}
